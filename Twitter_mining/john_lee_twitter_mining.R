b6addf6a0775ace4ddc59ff278cd91754d0b0950
John <jlee600@illinois.edu>
2017-11-05 19:56:32 -0600

    added twitter data folder

---
+++ Twitter_mining/john_lee_twitter_mining.R
@@ -0,0 +1,7 @@
+library(twitteR)
+
+api_key <- '2Sxgqqf0fpfFzjUCwoxfkQiMa'
+api_secret <- '	RquO4sxeLBp8o9yZH9I5haeyckY9dfTJb7mX180JjRZKV1Z4iQ'
+access_token <- '142134584-ULO3fBv7P96tPlnln8kclhA0oVSgIWKsktAUMZvQ'
+access_token_secret <- 'wKbTK1icm333EbAZvUNJ3euxQ9Pw75Fnhea8y8lZHjTKO'
+setup_twitter_oauth(api_key,api_secret)
