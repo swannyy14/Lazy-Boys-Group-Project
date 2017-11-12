d96ab472434f9a0370de7a20606d16948e4de060
Choi <jaewonc3@illinois.edu>
2017-11-12 16:41:41 -0600

    additional words texasshooting, nraisaterroristorganization, massshooting

--- code_jaewon_ews.R
+++ code_jaewon_ews.R
@@ -4,8 +4,11 @@ library(ROAuth, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downlo
 library(openssl, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downloads/STAT385 Statistical Programming Method/jaewon_library")
 library(httpuv, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downloads/STAT385 Statistical Programming Method/jaewon_library")
 library(base64enc, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downloads/STAT385 Statistical Programming Method/jaewon_library")
+library(bitops, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downloads/STAT385 Statistical Programming Method/jaewon_library")
 library(RCurl, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downloads/STAT385 Statistical Programming Method/jaewon_library")
 library(curl, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downloads/STAT385 Statistical Programming Method/jaewon_library")
+library(rjson, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downloads/STAT385 Statistical Programming Method/jaewon_library")
+
 
 download.file(url="http://curl.haxx.se/ca/cacert.pem",destfile="cacert.pem")
 options(RCurlOptions = list(cainfo = system.file("CurlSSL", "./cacert.pem", package = "RCurl")))
@@ -39,12 +42,55 @@ prayfortexas_5_6 <- twListToDF(prayfortexas_5_6)
 prayfortexas_6_7 <- searchTwitter("prayfortexas", since = '2017-11-06 ', until = '2017-11-07 ', n=32000)
 prayfortexas_6_7 <- twListToDF(prayfortexas_6_7)
 
-prayfortexas_7_8 <- searchTwitter("prayfortexas", since = '2017-11-06 ', until = '2017-11-07 ', n=32000)
+prayfortexas_7_8 <- searchTwitter("prayfortexas", since = '2017-11-07 ', until = '2017-11-08 ', n=32000)
 prayfortexas_7_8 <- twListToDF(prayfortexas_7_8)
 
 
+texasshooting_5_6 <- searchTwitter("texasshooting", since = '2017-11-05', until = '2017-11-06', n=32000)
+texasshooting_5_6 <- twListToDF(texasshooting_5_6)
+
+texasshooting_6_7 <- searchTwitter("texasshooting", since = '2017-11-06', until = '2017-11-07', n=32000)
+texasshooting_6_7 <- twListToDF(texasshooting_6_7)
+
+
+texasstrong_5_6 <- searchTwitter("texasstrong", since = '2017-11-05', until = '2017-11-06', n=32000)
+texasstrong_5_6 <- twListToDF(texasstrong_5_6)
+
+
+nra_4_5 <- searchTwitter("nraisaterroristorganization", since = '2017-11-04', until = '2017-11-05', n=3200)
+nra_4_5 <- twListToDF(nra_4_5)
+
+
+nra_5_6 <- searchTwitter("nraisaterroristorganization", since = '2017-11-05', until = '2017-11-06', n=3200)
+nra_5_6 <- twListToDF(nra_5_6)
+
+nra_6_7 <- searchTwitter("nraisaterroristorganization", since = '2017-11-06', until = '2017-11-07', n=3200)
+nra_6_7 <- twListToDF(nra_6_7)
+
+
+massshooting_5_6 <- searchTwitter("massshooting", since = '2017-11-05', until = '2017-11-06', n=20000)
+massshooting_5_6 <- twListToDF(massshooting_5_6)
+
+massshooting_6_7 <- searchTwitter("massshooting", since = '2017-11-06', until = '2017-11-07', n=20000)
+massshooting_6_7 <- twListToDF(massshooting_6_7)
+
+write.csv(massshooting_5_6,'massshooting_5_6.csv')
+write.csv(massshooting_6_7,'massshooting_6_7.csv')
+
 guncontrolnow_4_5 <- searchTwitter("guncontrolnow", since = '2017-11-04 ', until = '2017-11-05', n=32000)
 guncontrolnow_4_5 <- twListToDF(guncontrolnow_4_5)
+
+guncontrolnow_5_6 <- searchTwitter("guncontrolnow", since = '2017-11-05', until = '2017-11-06', n=32000)
+guncontrolnow_5_6 <- twListToDF(guncontrolnow_5_6)
+
+
+
+#-----------------json approach------------------------------
+library(streamR, lib.loc = "//ad.uillinois.edu/engr/instructional/jaewonc3/Downloads/STAT385 Statistical Programming Method/jaewon_library")
+my_oauth <- OAuthFactory$new(consumerKey=consumerKey,consumerSecret=consumerSecret, requestURL=reqURL,accessURL=accessURL, authURL=authURL)
+my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+
+filterStream( file.name="guncontrolnow.json", track="guncontrolnow", tweets=1000000, oauth=my_oauth )
 
 
 
